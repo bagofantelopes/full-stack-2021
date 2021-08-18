c5bea79507119f80d0e3af9404303ebe57b00c4e
bagofantelopes <agathangalos@gmail.com>
2021-08-18 14:47:07 -0400

    modified:   unicafe/src/App.js 	modified:   unicafe/src/index.js

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
 
-function App() {
+const Display = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>Good Ratings: {props.good}</h3>
+      <h3>Neutral Ratings: {props.neutral}</h3>
+      <h3>Bad Ratings: {props.bad}</h3>
     </div>
-  );
+  )
 }
 
-export default App;
+const Button = ({handleClick, text}) => {
+  return (
+    <button onClick={handleClick}>
+      {text}
+    </button>
+  )
+}
+const App = () => {
+  // save clicks of each button to its own useState
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const setToGood = good => {
+    setGood(good)
+  }
+
+  const setToNeutral = neutral => {
+    setNeutral(neutral)
+  }
+
+  const setToBad = bad => {
+    setBad(bad)
+  }
+
+  return (
+    <div>
+      <h1>User Feedback</h1>
+      <Button handleClick={() => setToGood(good+1)} text='good' />
+      <Button handleClick={() => setToNeutral(neutral+1)} text='neatral' />
+      <Button handleClick={() => setToBad(bad+1)} text='bad' />
+      <h1>Feedback Statistics</h1>
+      <div>
+        <Display good={good} neutral={neutral} bad={bad} />
+      </div>
+    </div>
+  )
+}
+
+export default App
